b71e21449bae3e994dd3c0557599c7ef22f1f8cb
Simone Alberto Peirone <simone.peirone.98@gmail.com>
2022-05-25 12:31:14 +0200

    Fix integration between the CelebA dataset and its implementation

--- celeba_dataset.py
+++ celeba_dataset.py
@@ -1,14 +1,17 @@
 import torch.utils.data as data
 from PIL import Image
 import torchvision.transforms as transforms
+from torchvision.transforms.functional import to_tensor
 from .base_dataset import BaseDataset
 
 from torchvision.datasets import CelebA
+from torchvision.transforms import ToTensor
 
 
-class CelebaDataset(CelebA, BaseDataset):
+class CelebaDataset(BaseDataset):
     def __init__(self):
         super(CelebaDataset, self).__init__()
+        self._dataset = None
 
     def name(self):
         return 'CelebaDataset'
@@ -17,9 +20,13 @@ class CelebaDataset(CelebA, BaseDataset):
     def modify_commandline_options(parser, is_train):
         return parser
 
-    def __getattribute__(self, name: str):
-        img, labels = super().__getattribute__(name)
-        return img, labels[-1]
+    def __getitem__(self, index: int):
+        img, label = self._dataset[index]
+        img = to_tensor(img)
+        return {'A': img, 'A_paths': label}
 
     def initialize(self, opt):
-        pass
+        self._dataset = CelebA(root=opt.dataroot)
+
+    def __len__(self):
+        return len(self._dataset)
